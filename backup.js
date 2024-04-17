360b244e01325fe33871075d42ebed232c606905
Juliaselmas <144105839+Juliaselmas@users.noreply.github.com>
2024-04-17 13:36:51 +0200

    WiP compare function

--- backup.js
+++ backup.js
@@ -2,7 +2,6 @@
 -Oavgjort om 4-4 i jämförelse?
 -Spara i localStorage?
 -Möjligheten att ta bort en pokemon?
--Maxantal 2?
 */
 
 let dropDownChoice = document.querySelector("#pokemon-dropdown");
@@ -17,16 +16,19 @@ let pokemonArray = [];
 let getPokemonData = async () => {
     let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
     let data = await response.json();
+    console.log(data);
     return data;
   };
 
   let createPokemonCollection = async () => {
     let pokemonData = await getPokemonData();
     pokemonArray = [];
+    //console.log(pokemonData);
 
     //loppar igenom varje pokemon i datan och skapar ett objekt av pokemon-klassen
     for (let pokemon of pokemonData.results) {
         let pokemonInfo = await getPokemonDetails(pokemon.url);
+        console.log(pokemonInfo);
         let pokemonCard = createPokemonCard(pokemonInfo);
         pokemonArray.push(pokemonCard);
     };
@@ -43,31 +45,31 @@ async function getPokemonDetails(url) {
 };
 
 function createPokemonCard(details) {
+   let data = details.data; // Uppdatera för att hämta 'data' från detaljobjektet
+
     let name = details.name;
     let imgUrl = details.sprites.front_default;
-    let types = details.types.map(type => type.type.name);
+    let types = details.types.map(typeInfo => typeInfo.type.name);
     let weight = details.weight;
     let height = details.height;
-    let stats = {};
-    for (let stat of details.stats) {
-        stats[stat.stat.name] = stat.base_stat;
-    }
+    let stats = {
+        hp: details.stats[0].base_stat,
+        attack: details.stats[1].base_stat,
+        specialAttack: details.stats[2].base_stat,
+        defense: details.stats[3].base_stat,
+        specialDefense: details.stats[4].base_stat,
+        speed: details.stats[5].base_stat
+    };
 
-    let statsString = "";
-    for (let stat in stats) {
-        // Formatera nyckel-värdepar för varje statistik
-        statsString += `<p>${stat.charAt(0).toUpperCase() + stat.slice(1)}: ${stats[stat]}</p>`;
-    }
-
-    return new Pokemon(name, imgUrl, types, weight, height, statsString);
-};
+    return new Pokemon(name, imgUrl, types, weight, height, stats);
+}
 
 
 createPokemonCollection().then(pokemonArray => {
     pokemonArray.forEach(pokemonCard => {
         let option = document.createElement("option");
-        option.value = pokemonCard.name;
-        option.text = pokemonCard.name.charAt(0).toUpperCase() + pokemonCard.name.slice(1);
+        option.value = pokemonCard.data.name;
+        option.text = pokemonCard.data.name.charAt(0).toUpperCase() + pokemonCard.data.name.slice(1);
         dropDownChoice.appendChild(option);
     });
 });
@@ -94,27 +96,34 @@ async function importToDropdown() {
 
 
 class Pokemon {
-    constructor(name, imgUrl, types, weight, height, stats) {
+    constructor(name, imgUrl, types, weight, height, data) {
         this.name = name;
-        this.imgUrl = imgUrl;
-        this.types = types;
-        this.weight = weight;
-        this.height = height;
-        this.stats = stats; // Nu en objekt istället för en sträng
+    this.imgUrl = imgUrl;
+    this.types = types;
+    this.weight = weight;
+    this.height = height;
+        this.stats = {
+            hp: data.stats[0].base_stat,
+            attack: data.stats[1].base_stat,
+            specialAttack: data.stats[2].base_stat,
+            defense: data.stats[3].base_stat,
+            specialDefense: data.stats[4].base_stat,
+            speed: data.stats[5].base_stat
+        };
     }
 
     static comparePokemon() {
         let pokemonCards = document.querySelectorAll(".pokemon-card");
     
         if (pokemonCards.length >= 2) {
-            let pokemonWins = Array(pokemonCards.length).fill(0); // Array för att lagra antalet vinster för varje pokemon
             let maxWins = 0; // Max antal vinster
+            let winnerIndex = -1; // Index för vinnande Pokémon-kort
     
             // Loopa igenom alla kombinationer av Pokemon-kort och jämför dem
             for (let i = 0; i < pokemonCards.length; i++) {
                 for (let j = i + 1; j < pokemonCards.length; j++) {
-                    let pokemon1 = pokemonArray.find(pokemon => pokemon.name === pokemonCards[i].querySelector("h3").textContent.toLowerCase());
-                    let pokemon2 = pokemonArray.find(pokemon => pokemon.name === pokemonCards[j].querySelector("h3").textContent.toLowerCase());
+                    let pokemon1 = pokemonCards[i].pokemon; // Hämta Pokémon-objektet från kortet
+                    let pokemon2 = pokemonCards[j].pokemon; // Hämta Pokémon-objektet från kortet
     
                     // Kontrollera om båda pokemons är giltiga
                     if (!pokemon1 || !pokemon2) {
@@ -125,76 +134,29 @@ class Pokemon {
                     let pokemon1Wins = 0;
                     let pokemon2Wins = 0;
     
-                    // Jämför HP-värdena och utför lämplig åtgärd baserat på resultatet
-                    let weightValue1 = parseInt(pokemon1.weight);
-                    let weightValue2 = parseInt(pokemon2.weight);
-                    let heightValue1 = parseInt(pokemon1.height);
-                    let heightValue2 = parseInt(pokemon2.height);
-                    let hpValue1 = parseInt(pokemon1.stats.hp);
-                    let hpValue2 = parseInt(pokemon2.stats.hp);
-                    let attackValue1 = parseInt(pokemon1.stats.attack);
-                    let attackValue2 = parseInt(pokemon2.stats.attack);
-                    let defenseValue1 = parseInt(pokemon1.stats.defense);
-                    let defenseValue2 = parseInt(pokemon2.stats.defense);
-                    let specAttackValue1 = parseInt(pokemon1.stats.specialattack);
-                    let specAttackValue2 = parseInt(pokemon2.stats.specialattack);
-                    let specDefenseValue1 = parseInt(pokemon1.stats.specialdefense);
-                    let specDefenseValue2 = parseInt(pokemon2.stats.specialdefense);
-                    let speedValue1 = parseInt(pokemon1.stats.speed);
-                    let speedValue2 = parseInt(pokemon2.stats.speed);
-    
-                    if (weightValue1 > weightValue2) {
-                        pokemon1Wins++; // Gör något om Pokemon 1 har högre HP än Pokemon 2
-                    } else if (weightValue1 < weightValue2) {
-                        pokemon2Wins++; // Gör något om Pokemon 2 har högre HP än Pokemon 1
-                    }
-                    if (heightValue1 > heightValue2) {
-                        pokemon1Wins++; // Gör något om Pokemon 1 har högre HP än Pokemon 2
-                    } else if (heightValue1 < heightValue2) {
-                        pokemon2Wins++; // Gör något om Pokemon 2 har högre HP än Pokemon 1
-                    }
-                    if (hpValue1 > hpValue2) {
-                        pokemon1Wins++; // Gör något om Pokemon 1 har högre HP än Pokemon 2
-                    } else if (hpValue1 < hpValue2) {
-                        pokemon2Wins++; // Gör något om Pokemon 2 har högre HP än Pokemon 1
-                    }
-                    if (attackValue1 > attackValue2) {
-                        pokemon1Wins++; // Gör något om Pokemon 1 har högre HP än Pokemon 2
-                    } else if (attackValue1 < attackValue2) {
-                        pokemon2Wins++; // Gör något om Pokemon 2 har högre HP än Pokemon 1
-                    }
-                    if (defenseValue1 > defenseValue2) {
-                        pokemon1Wins++; // Gör något om Pokemon 1 har högre HP än Pokemon 2
-                    } else if (defenseValue1 < defenseValue2) {
-                        pokemon2Wins++; // Gör något om Pokemon 2 har högre HP än Pokemon 1
-                    }
-                    if (specAttackValue1 > specAttackValue2) {
-                        pokemon1Wins++; // Gör något om Pokemon 1 har högre HP än Pokemon 2
-                    } else if (specAttackValue1 < specAttackValue2) {
-                        pokemon2Wins++; // Gör något om Pokemon 2 har högre HP än Pokemon 1
-                    }
-                    if (specDefenseValue1 > specDefenseValue2) {
-                        pokemon1Wins++; // Gör något om Pokemon 1 har högre HP än Pokemon 2
-                    } else if (specDefenseValue1 < specDefenseValue2) {
-                        pokemon2Wins++; // Gör något om Pokemon 2 har högre HP än Pokemon 1
-                    }
-                    if (speedValue1 > speedValue2) {
-                        pokemon1Wins++; // Gör något om Pokemon 1 har högre HP än Pokemon 2
-                    } else if (speedValue1 < speedValue2) {
-                        pokemon2Wins++; // Gör något om Pokemon 2 har högre HP än Pokemon 1
+                    for (let stat in pokemon1.stats) {
+                        let statValue1 = pokemon1.stats[stat];
+                        let statValue2 = pokemon2.stats[stat];
+                        
+                        if (parseInt(statValue1) > parseInt(statValue2)) {
+                            pokemon1Wins++;
+                        } else if (parseInt(statValue1) < parseInt(statValue2)) {
+                            pokemon2Wins++;
+                        }
                     }
     
-                    // Uppdatera antalet vinster för varje pokemon
-                    pokemonWins[i] += pokemon1Wins;
-                    pokemonWins[j] += pokemon2Wins;
-    
-                    // Uppdatera max antal vinster
-                    maxWins = Math.max(maxWins, pokemonWins[i], pokemonWins[j]);
+                    // Uppdatera max antal vinster och index för vinnande kort
+                    if (pokemon1Wins > pokemon2Wins && pokemon1Wins > maxWins) {
+                        maxWins = pokemon1Wins;
+                        winnerIndex = i;
+                    } else if (pokemon2Wins > pokemon1Wins && pokemon2Wins > maxWins) {
+                        maxWins = pokemon2Wins;
+                        winnerIndex = j;
+                    }
                 }
             }
     
-            // Hitta vinnaren med flest vinster
-            let winnerIndex = pokemonWins.indexOf(maxWins);
+            // Hämta det vinnande Pokémon-kortet
             let winnerPokemonCard = pokemonCards[winnerIndex];
     
             // Returnera det vinnande pokemon-kortet
@@ -206,8 +168,8 @@ class Pokemon {
 };
 
 class PokemonCard extends Pokemon {
-    constructor(name, imgUrl, types, weight, height, stats) {
-        super(name, imgUrl, types, weight, height, stats);
+    constructor(data) {
+        super(data); // Anropa superkonstruktören med data-objektet
         this.winner = false; // Ange initialt att kortet inte är vinnaren
     }
 
@@ -215,23 +177,22 @@ class PokemonCard extends Pokemon {
     render() {
         let pokemonCardDiv = document.createElement("div");
         pokemonCardDiv.classList.add("pokemon-card");
-
-        // Kontrollera vilka värden som är högre och lägre för att lägga till klasser för färg
-        let statsHtml = "";
-        for (let stat in this.stats) {
-            let statValue = this.stats[stat];
-            let statClass = parseInt(statValue) > 80 ? 'high-stat' : (parseInt(statValue) < 60 ? 'low-stat' : ''); // Anpassa dessa tröskelvärden efter önskemål
-            statsHtml += `<p class="${statClass}">${stat.charAt(0).toUpperCase() + stat.slice(1)}: ${statValue}</p>`;
-        }
-
+    
         pokemonCardDiv.innerHTML = `
-    <img src="${this.imgUrl}" alt="${this.name}">
-    <h3>${this.name.toUpperCase()}</h3>
-    <p>Types: ${this.types}</p>
-    <p>Weight: ${this.weight} hg</p>
-    <p>Height: ${this.height} dm</p>
-    <h4>Stats:</h4><p>${selectedPokemon.stats}</p>
-`;
+            <img src="${this.imgUrl}" alt="${this.name}">
+            <h3>${this.name.toUpperCase()}</h3>
+            <p>Types: ${this.types.join(", ")}</p>
+            <p>Weight: ${this.weight} hg</p>
+            <p>Height: ${this.height} dm</p>
+            <h4>Stats:</h4>
+            <p>HP: ${this.stats.hp}</p>
+            <p>Attack: ${this.stats.attack}</p>
+            <p>Defense: ${this.stats.defense}</p>
+            <p>Special Attack: ${this.stats.specialAttack}</p>
+            <p>Special Defense: ${this.stats.specialDefense}</p>
+            <p>Speed: ${this.stats.speed}</p>
+        `;
+    
         if (this.winner) {
             pokemonCardDiv.innerHTML += `<h3 class="winner-text">WINNER!</h3>`;
         }
@@ -242,7 +203,7 @@ class PokemonCard extends Pokemon {
 
 getCharacterBtn.addEventListener("click", async () => {
     let selectedPokemonName = dropDownChoice.value;
-    let selectedPokemon = pokemonArray.find(pokemon => pokemon.name === selectedPokemonName);
+    let selectedPokemon = pokemonArray.find(pokemon => pokemon.data.name === selectedPokemonName);
     console.log(selectedPokemonName);
 
     if (selectedPokemon) {
@@ -259,11 +220,17 @@ getCharacterBtn.addEventListener("click", async () => {
 
         pokemonCardDiv.innerHTML = `
         <img src="${selectedPokemon.imgUrl}" alt="${selectedPokemon.name}">
-        <h3>${selectedPokemon.name.toUpperCase()}</h3>
-        <p>Types: ${selectedPokemon.types}</p>
-        <p>Weight: ${selectedPokemon.weight} hg</p>
-        <p>Height: ${selectedPokemon.height} dm</p>
-        <h4>Stats:</h4><p>${selectedPokemon.stats}</p>
+        <h3>${selectedPokemon.data.name.toUpperCase()}</h3>
+        <p>Types: ${selectedPokemon.data.types.join(", ")}</p>
+        <p>Weight: ${selectedPokemon.data.weight} hg</p>
+        <p>Height: ${selectedPokemon.data.height} dm</p>
+        <h4>Stats:</h4>
+        <p>HP: ${selectedPokemon.stats.hp}</p>
+        <p>Attack: ${selectedPokemon.stats.attack}</p>
+        <p>Defense: ${selectedPokemon.stats.defense}</p>
+        <p>Special Attack: ${selectedPokemon.stats.specialAttack}</p>
+        <p>Special Defense: ${selectedPokemon.stats.specialDefense}</p>
+        <p>Speed: ${selectedPokemon.stats.speed}</p>
         `;
 
         pokemonContainer.appendChild(pokemonCardDiv);
@@ -280,6 +247,7 @@ getCharacterBtn.addEventListener("click", async () => {
             compareBtn.setAttribute('disabled', true);
         }
     } else {
+
         alert(`Unable to find information about ${selectedPokemonName}`);
     }
 });
